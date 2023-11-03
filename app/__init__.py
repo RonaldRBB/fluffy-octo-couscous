831b6fb83b73c9ef68b5e4a33a13ccab151d8d9d
Ronald Bello <ronaldbello2@gmail.com>
2023-11-03 20:18:20 -0300

    agregado cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,12 @@
 """App module"""
 from flask import Flask
-
+from flask_cors import CORS
 from app.routes.routes import setup_routes
 
 
 def create_app():
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__)
+    CORS(app)
     setup_routes(app)
     return app
