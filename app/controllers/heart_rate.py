44eb6080ae61cd872f850f11094a4979125b721e
Ronald Bello <ronaldbello2@gmail.com>
2023-11-09 20:49:34 -0300

    limpieza de codigo

--- app/controllers/heart_rate.py
+++ app/controllers/heart_rate.py
@@ -21,29 +21,18 @@ class HeartRate(Controller):
     def get_average(self):
         """Get average"""
         request_data = request.get_json()
-        start_date = request_data["start_date"]
-        end_date = request_data["end_date"]
-        start_date = datetime.strptime(start_date, "%Y-%m-%d")
-        end_date = datetime.strptime(end_date, "%Y-%m-%d")
-        print(start_date, end_date)
-        data = session.query(HeartRateModel).filter(
-            HeartRateModel._datetime.between(start_date, end_date)).all()
-        print(data)
-        array_data = []
-        # convert to pandas an get average of heart rate
-        df = pd.DataFrame(
-            [item.get_dict(with_relation=False) for item in data]
-        )
-        print(df)
-        # get average of heart rage
-        average = df["heart_rate"].mean()
-        print(average)
-        # round to two decimals
-        average = round(average, 2)
-        print(average)
-        for item in data:
-            # print(item.get_dict(with_relation=False))
-            array_data.append(item.get_dict(with_relation=False))
-        if not data:
-            return self.handle_response("not_found")
-        return self.handle_response("found_all", array_data)
+        start_date = datetime.strptime(request_data["start_date"], "%Y-%m-%d")
+        end_date = datetime.strptime(request_data["end_date"], "%Y-%m-%d")
+        try:
+            data = session.query(HeartRateModel).filter(
+                HeartRateModel._datetime.between(start_date, end_date)).all()
+            if not data:
+                return self.handle_response("not_found")
+            df = pd.DataFrame([item.get_dict(with_relation=False)
+                              for item in data])
+            average = round(df["heart_rate"].mean(), 2)
+            return self.handle_response("found_all", average)
+        except Exception as error:  # pylint: disable=W0703
+            session.rollback()
+            print(f" * {error}")
+            return self.handle_response("error")
